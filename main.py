322cfb0597ddc7834606769d22ffc77f543fb24e
AlexSoudant <asoudant@hotmail.fr>
2016-03-14 20:43:50 +0100

    Update Main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import app_config
 from libraries.twitterclient import TwitterClient
-
+from libraries.database_init import DataBase
 
 # Twitter API configuration
 consumer_key = app_config.twitter["consumer_key"]
@@ -9,8 +9,9 @@ consumer_secret = app_config.twitter["consumer_secret"]
 access_token = app_config.twitter["access_token"]
 access_token_secret = app_config.twitter["access_token_secret"]
 
-api = TwitterClient(consumer_key, consumer_secret, access_token, access_token_secret)
+db = DataBase()
 
+Tweet_db = TwitterClient(consumer_key, consumer_secret, access_token, access_token_secret)
 
 
 

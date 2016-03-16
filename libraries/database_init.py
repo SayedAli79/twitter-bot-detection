380fef373b903d6cd82abde5a356ed171c9799bb
Franck Brignoli <fbrignoli@gmail.com>
2016-03-16 20:49:30 +0100

    Fix: hardcoded tablename

--- libraries/database_init.py
+++ libraries/database_init.py
@@ -1,6 +1,5 @@
 import sqlite3
 
-
 class DataBase(object):
     def __init__(self, database_name,table_name):
         self.database_name = database_name
@@ -21,7 +20,7 @@ class DataBase(object):
 
     def feed_table(self, followers_name, tweet_date, tweet_text, tweet_mentions):
         conn = sqlite3.connect(self.database_name)
-        conn.execute("INSERT INTO TWEETS (NAME,DATE,TEXT,MENTIONS) VALUES (?,?,?,?)",
+        conn.execute("INSERT INTO {tn} (NAME,DATE,TEXT,MENTIONS) VALUES (?,?,?,?)".format(tn=self.table_name),
                      (followers_name, tweet_date, tweet_text, tweet_mentions))
 
         conn.commit()

3a3963a7592a5ea71aaa1aa7c45a2b116f02d476
AlexSoudant <asoudant@hotmail.fr>
2016-03-16 10:31:15 +0100

    Update database_init.py

--- libraries/database_init.py
+++ libraries/database_init.py
@@ -2,7 +2,7 @@ import sqlite3
 
 
 class DataBase(object):
-    def __init__(self, database_name):
+    def __init__(self, database_name,table_name):
         self.database_name = database_name
         self.table_name = table_name
 

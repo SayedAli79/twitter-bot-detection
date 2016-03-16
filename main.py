4041782e8ed48dd4b68e51fb546fd2d42ba3f766
Franck Brignoli <fbrignoli@gmail.com>
2016-03-16 20:57:28 +0100

    Move config file into config directory

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-import app_config
-from libraries.twitterclient import TwitterClient
+from config import app_config as cfg
 from libraries.database_init import DataBase
 from libraries.tweetimporter import TweetImporter
+from libraries.twitterclient import TwitterClient
 
 # Twitter API configuration
-consumer_key = app_config.twitter["consumer_key"]
-consumer_secret = app_config.twitter["consumer_secret"]
+consumer_key = cfg.twitter["consumer_key"]
+consumer_secret = cfg.twitter["consumer_secret"]
 
-access_token = app_config.twitter["access_token"]
-access_token_secret = app_config.twitter["access_token_secret"]
+access_token = cfg.twitter["access_token"]
+access_token_secret = cfg.twitter["access_token_secret"]
 
 # Start
-database = DataBase("bot_detection.db","TWEETS")
+database = DataBase(cfg.database["name"], cfg.database["tweet_table"])
 database.create_table()
 
 client = TwitterClient(consumer_key, consumer_secret, access_token, access_token_secret)
 
 importer = TweetImporter(client, database)
 importer.importData("FranckBrignoli")
-
-
 

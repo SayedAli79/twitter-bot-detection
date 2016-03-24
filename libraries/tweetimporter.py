bbf310e8c04af222791fe64dfa00d729719fa4e1
Franck Brignoli <fbrignoli@gmail.com>
2016-03-24 23:27:29 +0100

    Only user's tweets are imported by default, use --followers to import followers tweets

--- libraries/tweetimporter.py
+++ libraries/tweetimporter.py
@@ -5,29 +5,33 @@ class TweetImporter(object):
         self.twitter_client = twitter_client
         self.database = database
 
-    def importData(self, user, tweets_number=10):
+    def fromUser(self, user, tweets_number=10):
+        tweets = self.twitter_client.user_timeline(screen_name=user, count=tweets_number)
+        for i, status in enumerate(tweets):
+            tweet = status._json
+            text = tweet['text']
+            date = tweet['created_at']
+            entities = tweet['entities']
+            user_mentions = entities['user_mentions']
+            mentions_list = []
+
+            if len(user_mentions) > 0:
+                for mention in user_mentions:
+                    mentions_list.append(mention['screen_name'])
+
+            text_string = unicodedata.normalize('NFKD', text).encode('ascii','ignore')
+            date_string = unicodedata.normalize('NFKD', date).encode('ascii','ignore')
+            name_mentions_string = ",".join(mentions_list)
+
+            self.database.feed_table(
+                user,
+                text_string,
+                date_string,
+                name_mentions_string)
+
+    def fromFollowers(self, user, tweets_number=10):
         followers = self.twitter_client.followers_list(screen_name=user, count=200)
 
         for j, follower in enumerate(followers):
-            tweets = self.twitter_client.user_timeline(screen_name=follower, count=tweets_number)
-            for i, status in enumerate(tweets):
-                tweet = status._json
-                text = tweet['text']
-                date = tweet['created_at']
-                entities = tweet['entities']
-                user_mentions = entities['user_mentions']
-                mentions_list = []
+            self.fromUser(follower, tweets_number)
 
-                if len(user_mentions) > 0:
-                    for mention in user_mentions:
-                        mentions_list.append(mention['screen_name'])
-
-                text_string = unicodedata.normalize('NFKD', text).encode('ascii','ignore')
-                date_string = unicodedata.normalize('NFKD', date).encode('ascii','ignore')
-                name_mentions_string = ",".join(mentions_list)
-
-                self.database.feed_table(
-                    follower,
-                    text_string,
-                    date_string,
-                    name_mentions_string)
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 import argparse
 
 from config import app_config as cfg
@@ -6,7 +5,6 @@ from config import app_config as cfg
 from libraries.database_init import DataBase
 from libraries.tweetimporter import TweetImporter
 from libraries.twitterclient import TwitterClient
-from libraries.graphs.graph import Graph
 
 # Twitter API configuration
 consumer_key = cfg.twitter["consumer_key"]
@@ -15,21 +13,21 @@ consumer_secret = cfg.twitter["consumer_secret"]
 access_token = cfg.twitter["access_token"]
 access_token_secret = cfg.twitter["access_token_secret"]
 
+# Command line options
+parser = argparse.ArgumentParser(description='provide additional information to run the bot detection')
+parser.add_argument('specified_user', action="store", help='load tweets from the specified user')
+parser.add_argument('--followers', action="store_true", help="load tweets from user's followers")
+
+args = parser.parse_args()
+
 # Start
 database = DataBase(cfg.database["name"], cfg.database["tweet_table"])
+client = TwitterClient(consumer_key, consumer_secret, access_token, access_token_secret)
+importer = TweetImporter(client, database)
+
 database.create_table()
 
-client = TwitterClient(consumer_key, consumer_secret, access_token, access_token_secret)
+importer.fromUser(args.specified_user, 20)
+if args.followers:
+    importer.fromFollowers(args.specified_user, 20)
 
-parser = argparse.ArgumentParser(description='provide additional information to run the bot detection')
-parser.add_argument('specified_user', action="store",help='specify the twitter user to investigate')
-
-arg_results = parser.parse_args()
-importer = TweetImporter(client, database)
-importer.importData(arg_results.specified_user, 200)
-
-# dir = os.path.dirname(os.path.abspath(__file__))
-# out = "{}/images".format(dir)
-#
-# g = Graph(out)
-# g.avg_tweets(database.avg_mentions_per_user())

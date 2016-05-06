c21fbd514ee800e2502091649447577cecf4a335
Franck Brignoli <fbrignoli@gmail.com>
2016-05-06 20:42:41 +0200

    Fix: code formatting (space & tabs)

--- libraries/models.py
+++ libraries/models.py
@@ -8,14 +8,17 @@ from itertools import product
 
 db = SqliteDatabase(cfg.database["name"])
 
+
 def create_database():
     db.connect()
     db.drop_tables([User, Tweet], True)
     db.create_tables([User, Tweet], True)
 
+
 class BaseModel(Model):
     class Meta:
         database = db
+
 
 class User(BaseModel):
     screen_name = CharField()
@@ -34,6 +37,7 @@ class User(BaseModel):
         users = User.select().where(User.is_bot == is_bot)
 
         return [user.ratio_followers_following() for user in users]
+
 
 class Tweet(BaseModel):
     user = ForeignKeyField(User, related_name='tweets')
@@ -58,17 +62,15 @@ class Tweet(BaseModel):
 
     @classmethod
     def vocabulary_size(cls, is_bot=False, min_tweets=200):
-        selected_users = Tweet.select(Tweet.user)\
-            .group_by(Tweet.user)\
+        selected_users = Tweet.select(Tweet.user) \
+            .group_by(Tweet.user) \
             .having(fn.Count() >= min_tweets)
-
 
         tweets = (Tweet.select(Tweet).join(User)
             .where(
-                (User.is_bot == is_bot) &
-                (User.id << selected_users )
-            ))
-
+            (User.is_bot == is_bot) &
+            (User.id << selected_users)
+        ))
 
         words_per_user = defaultdict(lambda: set())
         for tweet in tweets:
@@ -76,54 +78,52 @@ class Tweet(BaseModel):
                 words_per_user[tweet.user_id].add(word)
 
         return {name: len(words) for (name, words) in words_per_user.iteritems()}
-    
+
     @classmethod
     def tweet_density(cls, is_bot=False, min_tweets=200):
-        selected_users = Tweet.select(Tweet.user)\
-            .group_by(Tweet.user)\
+        selected_users = Tweet.select(Tweet.user) \
+            .group_by(Tweet.user) \
             .having(fn.Count() >= min_tweets)
-
 
         tweets = (Tweet.select(Tweet).join(User)
             .where(
-                (User.is_bot == is_bot) &
-                (User.id << selected_users )
-            ))
+            (User.is_bot == is_bot) &
+            (User.id << selected_users)
+        ))
 
+        parsed_date = []
+        tweet_user = []
 
-	parsed_date = []
-	tweet_user = []
+        for i, tweet in enumerate(tweets):
+            tweet_user.append(tweet.user_id)
+            parsed_date.append(parser.parse(tweet.date))
 
-        for i,tweet in enumerate(tweets):
-        	tweet_user.append(tweet.user_id)
-        	parsed_date.append(parser.parse(tweet.date))
-	    
-	year_date = DataFrame(columns=["year","month","day"],index=range(len(parsed_date)))
+        year_date = DataFrame(columns=["year", "month", "day"], index=range(len(parsed_date)))
 
-	for i,date in enumerate(parsed_date):       	
-		year_date["year"][i] = date.year
-		year_date["month"][i] = date.month
-		year_date["day"][i] = date.day
-	
-	year_date["user_id"] = tweet_user 
+        for i, date in enumerate(parsed_date):
+            year_date["year"][i] = date.year
+            year_date["month"][i] = date.month
+            year_date["day"][i] = date.day
 
-	count_list_by_user = []
-	total_count_list = []
-	unique_users = list(set(year_date["user_id"]))
-	for user in unique_users:
-		year_date_by_user = year_date[year_date["user_id"] == user]
-		year = range(year_date_by_user["year"].min(),year_date_by_user["year"].max()+1)
-		month = range(1,13)
-		for y,m in product(year,month):
-			count = year_date_by_user["day"][year_date_by_user["year"] == y][year_date_by_user["month"] == m].value_counts()
-			for i in list(count):
-				if i < 6:
-					count_list_by_user.append(i)
-				else:
-					count_list_by_user.append(6)
-	
-	mean_count = np.mean(count_list_by_user)
-	median_count = np.median(count_list_by_user)
+        year_date["user_id"] = tweet_user
+
+        count_list_by_user = []
+        total_count_list = []
+        unique_users = list(set(year_date["user_id"]))
+        for user in unique_users:
+            year_date_by_user = year_date[year_date["user_id"] == user]
+            year = range(year_date_by_user["year"].min(), year_date_by_user["year"].max() + 1)
+            month = range(1, 13)
+            for y, m in product(year, month):
+                count = year_date_by_user["day"][year_date_by_user["year"] == y][
+                    year_date_by_user["month"] == m].value_counts()
+                for i in list(count):
+                    if i < 6:
+                        count_list_by_user.append(i)
+                    else:
+                        count_list_by_user.append(6)
+
+        mean_count = np.mean(count_list_by_user)
+        median_count = np.median(count_list_by_user)
 
         return count_list_by_user, mean_count, median_count
-

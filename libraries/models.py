a37c383c03bc6211409197bddce0da832fedd028
AlexSoudant <asoudant@hotmail.fr>
2016-05-06 10:53:55 +0200

    Update models.py

--- libraries/models.py
+++ libraries/models.py
@@ -73,4 +73,53 @@ class Tweet(BaseModel):
                 words_per_user[tweet.user_id].add(word)
 
         return {name: len(words) for (name, words) in words_per_user.iteritems()}
+    
+    @classmethod
+    def tweet_density(cls, is_bot=False, min_tweets=200):
+        selected_users = Tweet.select(Tweet.user)\
+            .group_by(Tweet.user)\
+            .having(fn.Count() >= min_tweets)
+
+
+        tweets = (Tweet.select(Tweet).join(User)
+            .where(
+                (User.is_bot == is_bot) &
+                (User.id << selected_users )
+            ))
+
+
+	parsed_date = []
+	tweet_user = []
+
+        for i,tweet in enumerate(tweets):
+	    tweet_user.append(tweet.user_id)
+            parsed_date.append(parser.parse(tweet.date))
+	    
+	year_date = DataFrame(columns=["year","month","day"],index=range(len(parsed_date)))
+
+	for i,date in enumerate(parsed_date):       	
+		year_date["year"][i] = date.year
+        	year_date["month"][i] = date.month
+        	year_date["day"][i] = date.day
+	
+	year_date["user_id"] = tweet_user 
+
+	count_list_by_user = []
+	total_count_list = []
+	unique_users = list(set(year_date["user_id"]))
+	for user in unique_users:
+		year_date_by_user = year_date[year_date["user_id"] == user]
+		for y in range(year_date_by_user["year"].min(),year_date_by_user["year"].max()+1):
+	    		for m in range(1,13):
+				count = year_date_by_user["day"][year_date_by_user["year"] == y][year_date_by_user["month"] == m].value_counts()
+				for i in list(count):
+					if i < 6:
+		    				count_list_by_user.append(i)
+					else:
+						count_list_by_user.append(6)
+	
+	mean_count = np.mean(count_list_by_user)
+	median_count = np.median(count_list_by_user)
+
+        return count_list_by_user, mean_count, median_count
 

afdd26d4f4ae5b65ba0cca52fe893feef3c0acf1
AlexSoudant <asoudant@hotmail.fr>
2016-06-23 22:49:18 +0200

    only call for reports when csv files from reports folder are missing

--- report.py
+++ report.py
@@ -2,6 +2,7 @@ import os
 from libraries.models import Tweet, User
 from config import app_config as cfg
 from libraries.graphs.graph import Graph
+import csv
 
 # Twitter API configuration
 consumer_key = cfg.twitter["consumer_key"]
@@ -9,7 +10,6 @@ consumer_secret = cfg.twitter["consumer_secret"]
 
 access_token = cfg.twitter["access_token"]
 access_token_secret = cfg.twitter["access_token_secret"]
-
 
 # Start
 current_path = os.path.dirname(os.path.abspath(__file__))
@@ -21,35 +21,88 @@ human_users = User.get_sample()
 bot_users = User.get_sample(True)
 
 # top sources
-path ="{}/images/top_sources.png".format(current_path)
-graph.top_sources(
-    Tweet.top_sources(human_tweets),
-    Tweet.top_sources(bot_tweets),
-    path
-)
+if os.path.isfile('reports/top_sources_human.csv') == False or os.path.isfile('reports/top_sources_bot.csv') == False:
+    top_sources_human = Tweet.top_sources(human_tweets)
+    top_sources_bot = Tweet.top_sources(bot_tweets)
+    list_top_sources_human = zip(top_sources_human.keys(),top_sources_human.values)
+    list_top_sources_bot = zip(top_sources_bot.keys(),top_sources_bot.values)
+    with open('reports/top_sources_human.csv', 'wb') as f:
+        writer = csv.writer(f)
+        for row in list_top_sources_human:
+            try:
+                writer.writerow(row)
+            except UnicodeEncodeError:
+                row = tuple(list(('UnicodeEncodeError',row[1])))  
+                writer.writerow(row)
+    with open('reports/top_sources_bot.csv', 'wb') as f:
+        writer = csv.writer(f)
+        for row in list_top_sources_bot:
+            try:
+                writer.writerow(row)
+            except UnicodeEncodeError:
+                row = tuple(list(('UnicodeEncodeError',row[1])))  
+                writer.writerow(row)
+    path ="{}/images/top_sources.png".format(current_path)
+    graph.top_sources(
+        top_sources_human,
+        top_sources_bot,
+        path
+    )
+
 
 # Average mentions per user
-path ="{}/images/avg_mentions.png".format(current_path)
-graph.avg_tweets(
-    Tweet.avg_mentions_per_user(human_tweets).values(),
-    Tweet.avg_mentions_per_user(bot_tweets).values(),
-    path
-)
+if os.path.isfile('reports/avg_mentions_per_human.csv') == False or os.path.isfile('reports/avg_mentions_per_bot.csv') == False:
+    avg_mentions_per_human = list(Tweet.avg_mentions_per_user(human_tweets).values())
+    avg_mentions_per_bot = list(Tweet.avg_mentions_per_user(bot_tweets).values())
+    with open('reports/avg_mentions_per_human.csv', 'wb') as f:
+        writer = csv.writer(f, delimiter = ',')
+        for row in avg_mentions_per_human:
+            writer.writerow([row])
+    with open('reports/avg_mentions_per_bot.csv', 'wb') as f:
+        writer = csv.writer(f)
+        for row in avg_mentions_per_bot:
+            writer.writerow([row])
+    path ="{}/images/avg_mentions.png".format(current_path)
+    graph.avg_tweets(
+        avg_mentions_per_human,
+        avg_mentions_per_bot,
+        path
+    )
 
 ## Vocabulary size per users
-path ="{}/images/vocabulary.png".format(current_path)
-graph.vocabulary(
-    Tweet.vocabulary_size(human_tweets).values(),
-    Tweet.vocabulary_size(bot_tweets).values(),
-    path
-)
+if os.path.isfile('reports/vocabulary_size_human.csv') == False or os.path.isfile('reports/vocabulary_size_bot.csv') == False:
+    vocabulary_size_human = list(Tweet.vocabulary_size(human_tweets).values())
+    vocabulary_size_bot = list(Tweet.vocabulary_size(bot_tweets).values())
+    with open('reports/vocabulary_size_human.csv', 'wb') as f:
+        writer = csv.writer(f, delimiter = ',')
+        for row in vocabulary_size_human:
+            writer.writerow([row])
+    with open('reports/vocabulary_size_bot.csv', 'wb') as f:
+        writer = csv.writer(f)
+        for row in vocabulary_size_bot:
+            writer.writerow([row])
+    path ="{}/images/vocabulary.png".format(current_path)
+    graph.vocabulary(
+        vocabulary_size_human,
+        vocabulary_size_bot,
+        path
+    )
 
 # number of tweets during active days
+if os.path.isfile('reports/tweet_density_per_human.csv') == False or os.path.isfile('reports/tweet_density_per_bot.csv') == False:
+    tweet_density_per_human, mean_count_user, median_count_user = Tweet.tweet_density(human_tweets)
+    tweet_density_per_bot, mean_count_bot, median_count_bot = Tweet.tweet_density(bot_tweets)
+    with open('reports/tweet_density_per_human.csv', 'wb') as f:
+        writer = csv.writer(f, delimiter = ',')
+        for row in tweet_density_per_human:
+            writer.writerow([row])
+    with open('reports/tweet_density_per_bot.csv', 'wb') as f:
+        writer = csv.writer(f)
+        for row in tweet_density_per_bot:
+            writer.writerow([row])
 path ="{}/images/density.png".format(current_path)
-tweet_density_per_user, mean_count_user, median_count_user = Tweet.tweet_density(human_tweets)
-tweet_density_per_bot, mean_count_bot, median_count_bot = Tweet.tweet_density(bot_tweets)
 graph.hist_density(
-    tweet_density_per_user,
+    tweet_density_per_human,
     tweet_density_per_bot,
     mean_count_user,
     median_count_user,
@@ -58,7 +111,19 @@ graph.hist_density(
     path
 )
 
+
 # number of tweets per week days
+if os.path.isfile('reports/tweet_weekday_human.csv') == False or os.path.isfile('reports/tweet_weekday_bot.csv') == False:
+    tweet_weekday_human = Tweet.tweet_weekday(human_tweets)
+    tweet_weekday_bot = Tweet.tweet_weekday(bot_tweets)
+    with open('reports/tweet_weekday_human.csv', 'wb') as f:
+        writer = csv.writer(f, delimiter = ',')
+        for row in tweet_weekday_human:
+            writer.writerow([row])
+    with open('reports/tweet_weekday_bot.csv', 'wb') as f:
+        writer = csv.writer(f)
+        for row in tweet_weekday_bot:
+            writer.writerow([row])
 path ="{}/images/weekdays.png".format(current_path)
 graph.hist_weekday(
     Tweet.tweet_weekday(human_tweets),
